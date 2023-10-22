ac371721301470dc97f84cd0c80c0641a03a6830
Alexpril <alexpril2002@gmail.com>
2023-10-22 15:00:09 +0100

    revert changes

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,7 +14,6 @@ namespace aiCorporation
         [STAThread]
         static void Main()
         {
-            Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
